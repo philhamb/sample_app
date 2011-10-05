7e018084269f625d05b14d1cd768bda38952ec9b
Phil <phil@phil-Aspire-4738.(none)>
2011-10-05 20:59:39 +0700

    Initial commit includes all gems for demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'fcc79e840e4b17fab9db40af4f9721afba7b63a4e76ee094801332a7f0daf269ecac3daeab24d91d10739153f1e6ec414ed66452c5d4040db9aae963abb6c3ad'
