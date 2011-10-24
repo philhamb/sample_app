190299999c38e9404c1b6fc8f7c108558aae857e
Phil Hambly <phil.hambly@gmail.com>
2011-10-24 22:13:25 +0700

    Up to changing layout links

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,10 +12,14 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      # Sign the user in and redirect to the user's show page.
+      sign_in user
+      redirect_to user
     end
   end  
+
   def destroy
+    sign_out
+    redirect_to root_path
   end
 end
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,6 @@ describe SessionsController do
   render_views
 
   describe "GET 'new'" do
-
     it "should be successful" do
       get :new
       response.should be_success
@@ -12,33 +11,63 @@ describe SessionsController do
     
     it "should have the right title" do
       get :new
-      response.should have_selector("title", :content => "Sign in")
+      response.should have_selector('title', :content => "Sign in")
     end
   end
-
+  
   describe "POST 'create'" do
+end
     
-    describe "invalid signin" do
-    
+    describe "failure" do
+      
       before(:each) do
-        @attr = { :email => "email@example.com", :password => "invalid" }
+        @attr = { :email => "", :password => "" }
       end
       
       it "should re-render the new page" do
         post :create, :session => @attr
         response.should render_template('new')
       end
-
+      
       it "should have the right title" do
         post :create, :session => @attr
-        response.should have_selector("title", :content => "Sign in")
+        response.should have_selector('title', :content => "Sign in")
       end
-
-      it "should have a flash.now message" do
+      
+      it "should have an error message" do
         post :create, :session => @attr
         flash.now[:error].should =~ /invalid/i
       end
     end
+    
+    describe "success" do
+      
+      before(:each) do
+        @user = Factory(:user)
+        @attr = { :email => @user.email, :password => @user.password }
+      end
+
+      it "should sign the user in" do
+        post :create, :session => @attr
+#        controller.current_user.should == @user
+        controller.should be_signed_in
+      end
+
+      it "should redirect to the user show page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
+#  end
+  
+  describe "DELETE 'destroy'" do
+    it "should sign a user out" do
+      test_sign_in(Factory(:user))
+      delete :destroy
+      controller.should_not be_signed_in
+      response.should redirect_to(root_path)
+    end
   end
 end
+
 
